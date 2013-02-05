89c9ebc186243a9ee010eed3530abfcc4ce35095
Rich Boakes <rich.boakes@port.ac.uk>
2013-02-05 01:41:59 +0000

    initial build

---
+++ src/uoptoc.js
@@ -0,0 +1,101 @@
+// Usage: see github readme.md
+var uoptoc = ( function x() {
+	"use strict";
+
+	// all methods are defined as vars and are thus private,
+	// only those returned at the end become public
+
+	var //... note the use of commas at the end of each function def
+		// so that only one var is used.
+
+	/**
+	 * A given node is checked to see if it has an ID attribute and
+	 * if none is found, one is added.  The ID generated is based
+	 * on the content of the first H1 element within the node or
+	 * if there is no H1 elemenet, the nodes text content.
+	 * Uniqueness of new IDs ensured by appending underscores.
+	 */
+	ensureNodeHasId = function (node) {
+		var firstHeading, text;
+		if (node.id === "") {
+			firstHeading = node.querySelector("h1");
+			text = firstHeading ? firstHeading.innerHTML: node.innerHTML;
+			text = text.substring(0,40).replace(/[^a-z]+/gi,"").toLowerCase();
+			text = text.substring(0,20);
+			while (document.getElementById(text)) {
+				text += "_";
+			}
+			node.id = text;
+		}
+	},
+	
+	/*
+	 * Look within the DOM any sections that contain elements with both
+	 * input and output classes
+	 */
+	treewalk = function(node, cont) {
+		var i, localheadings, ul, li, a;
+
+		if (cont && node.children) {
+			ul = document.createElement('ul');
+			cont.appendChild(ul);
+			for (i = 0; i < node.children.length; ++i) {
+				ensureNodeHasId(node.children[i]);
+				if (node.children[i].nodeName.toLowerCase() === "section" ) {
+					 
+					a = document.createElement('a');
+					localheadings = node.children[i].querySelectorAll("h1");
+					if (localheadings[0]) {
+						a.innerHTML = localheadings[0].innerHTML;
+						a.href= "#"+node.children[i].id;
+						li = document.createElement('li');
+						li.appendChild(a);
+						ul.appendChild(li);
+						treewalk(node.children[i], li);
+					}
+				}
+			}
+		}
+		return cont;
+	},
+
+	/*
+	 * Look within the DOM any sections that contain elements with both
+	 * input and output classes
+	 */
+	gen = function(startNode) {
+		if (startNode) {
+			return treewalk(startNode, document.createElement("div") );
+		}
+		throw "startNode not defined";
+	},
+
+	/*
+	 * Look within the DOM any sections that contain elements with both
+	 * input and output classes
+	 */
+	genPop = function(startNode, destNode) {
+		var tree = gen(startNode);
+		if (destNode) {
+			destNode.appendChild( tree );
+			return tree;
+		}
+		throw "destNode not defined";
+	},
+
+	genPopSelector = function(startSelector, destSelector) {
+		return genPop(
+			document.querySelector(startSelector),
+			document.querySelector(destSelector)
+		);
+	};
+
+
+	/* return methods that should be public */
+	return {
+		gen: gen,
+		genPop: genPop,
+		genPopSelector: genPopSelector
+	};
+
+} ());
---
+++ test/test.js
@@ -0,0 +1,76 @@
+test("gen", 15, function() {
+
+	var parent = document.querySelector("article");
+	var tree = uoptoc.gen(parent);
+
+	ok( tree , "The gen function should return a tree: " + tree );
+	equal( typeof tree, "object", "The gen function returns an object." );
+
+	var anchors = tree.querySelectorAll("a");
+	equal( anchors.length, 6, "There should be six anchors." );
+
+	// nb a turkey is a thing that gets stuffed.  in this case with
+	// properies whose name we want to check for uniqueness.  Note the
+	// use of the double negative !! which casts the object to a negative
+	// boolean truthy value then back.
+	var turkey = {};
+	for (var i = anchors.length - 1; i >= 0; i--) {
+		var a = anchors.item(i);
+		notEqual( !!turkey[a], true, "The element "+a+", is not yet present." );
+		turkey[a] = "irrelevant";
+		equal( !!turkey[a], true, "The element "+a+", is now present." );
+	}
+
+});
+
+test("genPop", 15, function() {
+
+	var parent = document.querySelector("article");
+	var dest = document.querySelector("#qunit-fixture");
+	var tree = uoptoc.genPop(parent, dest);
+
+	ok( tree , "The gen function should return a tree: " + tree );
+	equal( typeof tree, "object", "The gen function returns an object." );
+
+	// note the use of dest here to check the content is in the page
+	var anchors = dest.querySelectorAll("a");
+	equal( anchors.length, 6, "There should be six anchors." );
+
+	// nb a turkey is a thing that gets stuffed.  in this case with
+	// properies whose name we want to check for uniqueness.  Note the
+	// use of the double negative !! which casts the object to a negative
+	// boolean truthy value then back.
+	var turkey = {};
+	for (var i = anchors.length - 1; i >= 0; i--) {
+		var a = anchors.item(i);
+		notEqual( !!turkey[a], true, "The element "+a+", is not yet present." );
+		turkey[a] = "irrelevant";
+		equal( !!turkey[a], true, "The element "+a+", is now present." );
+	}
+
+});
+
+test("genPopSelector", 15, function() {
+
+	var tree = uoptoc.genPopSelector("article", "#qunit-fixture");
+
+	ok( tree , "The gen function should return a tree: " + tree );
+	equal( typeof tree, "object", "The gen function returns an object." );
+
+	// note the use of dest here to check the content is in the page
+	var anchors = tree.querySelectorAll("a");
+	equal( anchors.length, 6, "There should be six anchors." );
+
+	// nb a turkey is a thing that gets stuffed.  in this case with
+	// properies whose name we want to check for uniqueness.  Note the
+	// use of the double negative !! which casts the object to a negative
+	// boolean truthy value then back.
+	var turkey = {};
+	for (var i = anchors.length - 1; i >= 0; i--) {
+		var a = anchors.item(i);
+		notEqual( !!turkey[a], true, "The element "+a+", is not yet present." );
+		turkey[a] = "irrelevant";
+		equal( !!turkey[a], true, "The element "+a+", is now present." );
+	}
+
+});
